3684020f7cba43507fc50c1fa8d522480367ef43
Dimas Toha Pramawitra <lonehack@users.noreply.github.com>
2016-08-19 00:03:52 +0700

    Update Member.php

--- application/models/Member.php
+++ application/models/Member.php
@@ -7,8 +7,11 @@ class Member extends Base_model {
 	    parent::__construct();          
 	}
 	function getData(){
-		$this->db->select("city_name(city_id) AS city_name, region_name(region) as region_name, member.*");		
-		$result=$this->db->get('member');
+		$this->db->select("cities.city_name, regions.reg_name, member.*");		
+		$this->db->from('member');
+		$this->db->join('cities', 'member.city_id = cities.city_id');
+		$this->db->join('regions', 'cities.reg_id = regions.reg_id');
+		$result=$this->db->get();
 		return $result->result_array();
 	}
 	function update($id,$data){
@@ -16,7 +19,13 @@ class Member extends Base_model {
 		$this->db->update($this->table,$data);
 	}
 	function getTrip(){
-		$query="SELECT city_name(start_city) AS start, city_name(destinate) as finish,trip.* FROM trip where trip_id IN (SELECT trip_id from partisipant where user_id='".$_SESSION['user']['user_id']."')";
+		$query="SELECT *
+			FROM `trip_view` look
+    			WHERE look.`trip_id` IN
+			(
+				SELECT `partisipant`.`trip_id` 
+				FROM `partisipant` 
+				WHERE `partisipant`.`user_id` ='".$_SESSION['user']['user_id']."')";
 		$result=$this->db->query($query);
 		return $result->result_array();
 	}

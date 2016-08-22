c2820b3f1bcaa186c6bcad35eb9b83e3c4404231
Dimas Toha Pramawitra <lonehack@users.noreply.github.com>
2016-08-23 01:38:22 +0700

    Update TripModel.php

--- application/models/TripModel.php
+++ application/models/TripModel.php
@@ -8,20 +8,22 @@ class TripModel extends Base_model {
 	}
 	function search($data){
 		$query='SELECT * FROM trip_view 
-				WHERE start_country LIKE ("%'.$data["search"].'%") 
-					OR start_city LIKE ("%'.$data["search"].'%") 
-        				OR dest_country LIKE ("%'.$data["search"].'%") 
-        				OR dest_reg LIKE ("%'.$data["search"].'%") 
-        				OR dest_city LIKE ("%'.$data["search"].'%")
-        				OR destinate LIKE ("%'.$data["search"].'%")
+				WHERE flags = 1;
 					AND timeheld >= NOW()
+					AND destinate LIKE ("%'.$data["search"].'%")
+					OR dest_city LIKE ("%'.$data["search"].'%")
+        				OR dest_reg LIKE ("%'.$data["search"].'%") 
+        				OR dest_country LIKE ("%'.$data["search"].'%")
+					OR start_city LIKE ("%'.$data["search"].'%")
+					OR start_reg LIKE ("%'.$data["search"].'%")
+					OR start_country LIKE ("%'.$data["search"].'%") 
 					OR trip_name LIKE ("%'.$data["search"].'%");';
-		
 		$result=$this->db->query($query);
 		return $result->result_array();
 	}
 	function getBy($data){
 		$this->db->select("trip_view.*");
+		$this->db->where('flags',1);
 		$query=$this->db->get_where('trip_view',$data);
 		return $query->result_array();
 	}
